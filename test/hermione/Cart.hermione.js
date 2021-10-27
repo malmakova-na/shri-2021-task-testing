6ce63faac4c981d48c2f47d4f60c59203afac5c3
malmakova.na <malmakova.na@phystech.edu>
2021-10-28 01:33:14 +0300

    update

--- test/hermione/Cart.hermione.js
+++ test/hermione/Cart.hermione.js
@@ -9,25 +9,5 @@ describe('Тест на корзину', async function() {
         compositeImage: true,
         });
     });
-    it("очищение ", async function() {
-        await browser.url('https://shri.yandex/hw/store');
-
-        const menu = await browser.$('.navbar-nav');
-        await menu.$$('a')[0].click();
-
-        await browser.$('.card-link').click();
-
-        await browser.$('.btn').click();
-
-        await menu.$$('a')[3].click();
-
-        assert.equal(await browser.$('.table').isDisplayed(), true);
-        assert.equal(await browser.$('.Form').isDisplayed(), true);
-
-        await browser.$('.Cart-Clear').click();
-
-        assert.equal(await browser.$('.table').isDisplayed(), false);
-        assert.equal(await browser.$('.Form').isDisplayed(), false);
-        assert.equal(await menu.$$('a')[3].getText(), 'Cart');
-    })
+   
 });
